a2ee763e15a79174e7189bb428ff1f0590b92fad
Paul Irish <paul.irish@gmail.com>
2017-04-15 01:41:53 -0700

    serious cleanup of runner. -G -A -R aliases.
    BTW the networkrecords bit is cherrypickable.

--- lighthouse-core/config/config.js
+++ lighthouse-core/config/config.js
@@ -285,11 +285,6 @@ class Config {
     this._configDir = configPath ? path.dirname(configPath) : undefined;
 
     this._passes = configJSON.passes || null;
-    this._auditResults = configJSON.auditResults || null;
-    if (this._auditResults && !Array.isArray(this._auditResults)) {
-      throw new Error('config.auditResults must be an array');
-    }
-
     this._aggregations = configJSON.aggregations || null;
 
     this._audits = Config.requireAudits(configJSON.audits, this._configDir);
@@ -512,6 +507,10 @@ class Config {
     });
   }
 
+  removePasses() {
+    this._passes = undefined;
+  }
+
   /** @type {string} */
   get configDir() {
     return this._configDir;
@@ -525,11 +524,6 @@ class Config {
   /** @type {Array<!Audit>} */
   get audits() {
     return this._audits;
-  }
-
-  /** @type {Array<!AuditResult>} */
-  get auditResults() {
-    return this._auditResults;
   }
 
   /** @type {Array<!Artifacts>} */
